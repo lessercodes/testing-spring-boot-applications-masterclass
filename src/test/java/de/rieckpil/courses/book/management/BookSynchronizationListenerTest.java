1cd5eb03de9f635a2ff079f60c3e76f6b4677cf9
Csaba Szabó (@46666) <46666@4ig.hu>
2025-02-07 16:11:58 +0100

    Finished - Introduction to Mockito

--- src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,14 +23,44 @@ class BookSynchronizationListenerTest {
   @Captor private ArgumentCaptor<Book> bookArgumentCaptor;
 
   @Test
-  void shouldRejectBookWhenIsbnIsMalformed() {}
+  void shouldRejectBookWhenIsbnIsMalformed() {
+    final var bookSynchronization = new BookSynchronization("12345");
+    cut.consumeBookUpdates(bookSynchronization);
+    verifyNoInteractions(bookRepository, openLibraryApiClient);
+  }
 
   @Test
-  void shouldNotOverrideWhenBookAlreadyExists() {}
+  void shouldNotOverrideWhenBookAlreadyExists() {
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(new Book());
+    final var bookSynchronization = new BookSynchronization(VALID_ISBN);
+    cut.consumeBookUpdates(bookSynchronization);
+    verifyNoInteractions(openLibraryApiClient);
+    verify(bookRepository, never()).save(any());
+  }
 
   @Test
-  void shouldThrowExceptionWhenProcessingFails() {}
+  void shouldThrowExceptionWhenProcessingFails() {
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(null);
+    when(openLibraryApiClient.fetchMetadataForBook(VALID_ISBN)).thenThrow(new RuntimeException("Network timeout"));
+    final var bookSynchronization = new BookSynchronization(VALID_ISBN);
+    assertThrows(RuntimeException.class, () -> cut.consumeBookUpdates(bookSynchronization));
+  }
 
   @Test
-  void shouldStoreBookWhenNewAndCorrectIsbn() {}
+  void shouldStoreBookWhenNewAndCorrectIsbn() {
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(null);
+
+    final var requestedBook = new Book();
+    requestedBook.setIsbn(VALID_ISBN);
+    requestedBook.setTitle("Java book");
+    when(openLibraryApiClient.fetchMetadataForBook(VALID_ISBN)).thenReturn(requestedBook);
+
+    final var bookSynchronization = new BookSynchronization(VALID_ISBN);
+    cut.consumeBookUpdates(bookSynchronization);
+
+    verify(bookRepository).save(bookArgumentCaptor.capture());
+
+    assertEquals(VALID_ISBN, bookArgumentCaptor.getValue().getIsbn());
+    assertEquals("Java book", bookArgumentCaptor.getValue().getTitle());
+  }
 }
